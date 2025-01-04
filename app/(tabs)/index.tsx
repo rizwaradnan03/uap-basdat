fc8d083ca83996899cd654964395153cd580d2c8
rizwaradnan03 <rizwar.adnanbyantara@gmail.com>
2025-01-04 14:19:01 +0700

    adding toast

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { Button, Input } from '@ui-kitten/components'
+import { ScreenWidth } from '@/config'
 
 const UserScreen = () => {
+  const [username, setUsername] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+
+  const handleSubmit = async () => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
   return (
-    <View>
-      <Text>UserScreen</Text>
-    </View>
+    <>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{flex: 0, flexDirection: 'column', gap: 15, width: ScreenWidth * 0.8}}>
+          <Text style={{fontSize: 20, textAlign: 'center'}}>Input Pengguna</Text>
+          <Input value={username} onChangeText={(value) => setUsername(value)} placeholder='Username' />
+          <Input value={password} onChangeText={(value) => setPassword(value)} placeholder='Password' />
+          <Button>Submit</Button>
+        </View>
+      </View>
+    </>
   )
 }
 
